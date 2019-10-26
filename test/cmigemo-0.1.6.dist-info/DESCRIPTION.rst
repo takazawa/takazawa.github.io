b17f54e4a7a3685dd5721ff5b7de13097fc9f37e
Yotaro Takazawa <abcdeyo@gmail.com>
2019-10-26 14:24:21 +0900

    setup hexo

--- test/cmigemo-0.1.6.dist-info/DESCRIPTION.rst
+++
@@ -1,3 +0,0 @@
-A C/Migemo binding for python written in pure python using ctypes.
-
-
--- test/cmigemo/info.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__doc__ = """A C/Migemo binding for python written in pure python using ctypes."""
-__version__ = "0.1.6"
